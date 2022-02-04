37ba6e6e348c0988cce902979c28fe15e438def0
MDedja <milandedjanski@gmail.com>
2022-02-04 19:05:24 +0100

    Sanctum autentifikacija rest apia

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\API\TerController;
 use App\Http\Controllers\API\TrenerController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,11 +17,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::resource('teretana', TerController::class);
-Route::resource('trener', TrenerController::class);
 
 
+Route::post('register', [AuthController::class, 'register']);
+Route::post('login', [AuthController::class, 'login']);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::resource('teretana', TerController::class);
+    Route::resource('trener', TrenerController::class);
+    Route::post('logout', [AuthController::class, 'logout']);
 });
